57f770c1a727925698d6922be4c97cdcad6196ae
Gastón Ariel Fauret Caceres <gastonfauret@gmail.com>
2023-09-26 10:52:17 -0300

    Pagina Lista a la espera de la Aprobacion del cliente

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -3,6 +3,7 @@ function copiar(clase, contenido) {
     const textoCopiado = document.querySelector(clase);
 
     navigator.clipboard.writeText(textoCopiado.innerText);
-    alert(`${contenido} copiado.`);
+    //alert(`${contenido} copiado.`);
+    swal(`${contenido} copiado correctamente.`, "Gracias por Colaborar", "success");
 }
 
